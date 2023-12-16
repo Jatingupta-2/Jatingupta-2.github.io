2e35509a1d3354fe0f3215cb11e6c1b09920356c
Jatin Gupta <38355753+Jatingupta-2@users.noreply.github.com>
2023-12-16 22:26:28 +0530

    changes

--- shared.js
+++ shared.js
@@ -1,4 +1,5 @@
 var backdrop = document.querySelector(".backdrop");
+var backdropClose = document.querySelector("#backdropClose");
 var mobileNav = document.querySelector(".mobile-nav");
 var toggleButton = document.querySelector("#toggle-mobile-nav");
 var toggleButton1 = document.querySelector("#toggle-mobile-nav1");
@@ -11,9 +12,9 @@ var closeMoal = document.querySelectorAll(".close-modal");
 
   
 toggleButton.addEventListener("click", openMobileNev)
-toggleButton1.addEventListener("click", openMobileNev)
-toggleButton2.addEventListener("click", openMobileNev)
-toggleButton3.addEventListener("click", openMobileNev)
+toggleButton1?.toggleButton1.addEventListener("click", openMobileNev)
+toggleButton1?.toggleButton2.addEventListener("click", openMobileNev)
+toggleButton1?.toggleButton3.addEventListener("click", openMobileNev)
 
 
 function openMobileNev() {
@@ -22,12 +23,13 @@ function openMobileNev() {
 }
 
 
-backdrop.addEventListener("click", function () {
-  
+backdrop.addEventListener("click", closeMobileNav);
+
+
+function closeMobileNav() {
   mobileNav.classList.remove("open");
   backdrop.classList.remove("open");
-
-});
+}
 
 closeMoal.forEach(e => {
   e.addEventListener("click", function () {
